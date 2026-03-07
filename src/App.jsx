ec1e66afd3da00712b2cfb9563b9481885a5f339
roméo <r.lamarra@herewecom.fr>
2026-03-07 17:04:21 +0100

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,25 @@ import Hero from "./components/hero";
 import NavBar from "./components/NavBar";
 import ProductViewer from "./components/ProductViewer";
 import gsap from "gsap";
-import { ScrollTrigger, SplitText } from "gsap/all";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText";
 import Showcase from "./components/Showcase";
+import Features from "./components/Features";
+import Footer from "./components/Footer";
+import PerformanceMac from "./components/PerformanceMac";
+import Hightlights from "./components/Hightlights";
+import { useEffect } from "react";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
+
+    useEffect(() => {
+        // Rafraîchir quand toutes les images du site sont chargées
+        window.addEventListener("load", () => ScrollTrigger.refresh());
+        
+        return () => window.removeEventListener("load", () => ScrollTrigger.refresh());
+    }, []);
 
     return (
         <main>
@@ -15,6 +28,10 @@ const App = () => {
             <Hero/>
             <ProductViewer/>
             <Showcase/>
+            <PerformanceMac/>
+            <Features/>
+            <Hightlights/>
+            <Footer/>
         </main>
     )
 }
